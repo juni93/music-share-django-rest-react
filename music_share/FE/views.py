28481bb1bd20cf2c0eb4dcbbf9e2d437b9069695
unknown <juni.93.juni@gmail.com>
2020-11-29 17:28:01 +0100

    setup Frontend with react and configuration of Babel and Webpack

--- music_share/FE/views.py
+++ music_share/FE/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#render the view from django and let REACT handle all FE
+def index(request, *args, **kwargs):
+    return render(request, 'frontend/index.html')
